544312e5fe84aa74151e2699d23390bb2c5e62cf
lvl0nax <lvl0nax@gmail.com>
2014-03-15 12:23:28 +0400

    fix little bug

--- app/admin/pictures.rb
+++ app/admin/pictures.rb
@@ -2,7 +2,7 @@ ActiveAdmin.register Picture do
   index do
     column :title
     column :image do |photo|
-      link_to(image_tag(photo.image.url(:thumb)), admin_photo_path(photo))
+      link_to(image_tag(photo.image.url(:thumb)), admin_picture_path(photo))
     end
     column :alt
     column :created_at
