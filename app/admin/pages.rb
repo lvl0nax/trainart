79c4950ba358a9a9c1e64849f09aceb7177d6f7e
lvl0nax <lvl0nax@gmail.com>
2014-04-27 16:44:29 +0400

    fixing active_admin redactor

--- app/admin/pages.rb
+++ app/admin/pages.rb
@@ -2,7 +2,7 @@ ActiveAdmin.register Page do
   form do |f|
     f.inputs do
       f.input :title
-      f.input :body, as: :html_editor
+      f.input :body, input_html:{class: 'redactor'}
       f.input :mtitle
       f.input :mkeywords
       f.input :mdesc
